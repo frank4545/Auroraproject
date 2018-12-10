8c4dcbaa19bdb4a652de502b3d5e374e1b6bd74f
frank4545 <45745524+frank4545@users.noreply.github.com>
2018-12-09 20:00:24 -0600

    Add files via upload

---
+++ Chapter8.py
@@ -0,0 +1,239 @@
+def display_message():
+    """Display a message about what I'm learning."""
+    msg = "I'm learning to store code in functions."
+    print(msg)
+
+display_message()
+
+def favorite_book(title):
+    """Display a message about someone's favorite book."""
+    print(title + " is one of my favorite books.")
+
+favorite_book('The Abstract Wild')
+
+def make_shirt(size, message):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
+
+make_shirt('large', 'I love Python!')
+make_shirt(message="Readability counts.", size='medium')
+
+def make_shirt(size='large', message='I love Python!'):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Programmers are loopy.')
+
+def describe_city(city, country='chile'):
+    """Describe a city."""
+    msg = city.title() + " is in " + country.title() + "."
+    print(msg)
+
+describe_city('santiago')
+describe_city('reykjavik', 'iceland')
+describe_city('punta arenas')
+
+def city_country(city, country):
+    """Return a string like 'Santiago, Chile'."""
+    return(city.title() + ", " + country.title())
+
+city = city_country('santiago', 'chile')
+print(city)
+
+city = city_country('ushuaia', 'argentina')
+print(city)
+
+city = city_country('longyearbyen', 'svalbard')
+print(city)
+
+def make_album(artist, title):
+    """Build a dictionary containing information about an album."""
+    album_dict = {
+        'artist': artist.title(),
+        'title': title.title(),
+        }
+    return album_dict
+
+album = make_album('metallica', 'ride the lightning')
+print(album)
+
+album = make_album('beethoven', 'ninth symphony')
+print(album)
+
+album = make_album('willie nelson', 'red-headed stranger')
+print(album)
+
+def make_album(artist, title, tracks=0):
+    """Build a dictionary containing information about an album."""
+    album_dict = {
+        'artist': artist.title(),
+        'title': title.title(),
+        }
+    if tracks:
+        album_dict['tracks'] = tracks
+    return album_dict
+
+album = make_album('metallica', 'ride the lightning')
+print(album)
+
+album = make_album('beethoven', 'ninth symphony')
+print(album)
+
+album = make_album('willie nelson', 'red-headed stranger')
+print(album)
+
+album = make_album('iron maiden', 'piece of mind', tracks=8)
+print(album)
+
+
+def make_album(artist, title, tracks=0):
+    """Build a dictionary containing information about an album."""
+    album_dict = {
+        'artist': artist.title(),
+        'title': title.title(),
+    }
+    if tracks:
+        album_dict['tracks'] = tracks
+    return album_dict
+
+
+# Prepare the prompts.
+title_prompt = "\nWhat album are you thinking of? "
+artist_prompt = "Who's the artist? "
+
+# Let the user know how to quit.
+print("Enter 'quit' at any time to stop.")
+
+while True:
+    title = input(title_prompt)
+    if title == 'quit':
+        break
+
+    artist = input(artist_prompt)
+    if artist == 'quit':
+        break
+
+    album = make_album(artist, title)
+    print(album)
+
+print("\nThanks for responding!")
+
+def show_magicians(magicians):
+    """Print the name of each magician in the list."""
+    for magician in magicians:
+        print(magician.title())
+
+magicians = ['harry houdini', 'david blaine', 'teller']
+show_magicians(magicians)
+
+def show_magicians(magicians):
+    """Print the name of each magician in the list."""
+    for magician in magicians:
+        print(magician)
+
+def make_great(magicians):
+    """Add 'the Great!' to each magician's name."""
+    # Build a new list to hold the great musicians.
+    great_magicians = []
+
+    # Make each magician great, and add it to great_magicians.
+    while magicians:
+        magician = magicians.pop()
+        great_magician = magician + ' the Great'
+        great_magicians.append(great_magician)
+
+    # Add the great magicians back into magicians.
+    for great_magician in great_magicians:
+        magicians.append(great_magician)
+
+magicians = ['Harry Houdini', 'David Blaine', 'Teller']
+show_magicians(magicians)
+
+print("\n")
+make_great(magicians)
+show_magicians(magicians)
+
+def show_magicians(magicians):
+    """Print the name of each magician in the list."""
+    for magician in magicians:
+        print(magician)
+
+def make_great(magicians):
+    """Add 'the Great!' to each magician's name."""
+    # Build a new list to hold the great musicians.
+    great_magicians = []
+
+    # Make each magician great, and add it to great_magicians.
+    while magicians:
+        magician = magicians.pop()
+        great_magician = magician + ' the Great'
+        great_magicians.append(great_magician)
+
+    # Add the great magicians back into magicians.
+    for great_magician in great_magicians:
+        magicians.append(great_magician)
+
+    return magicians
+
+magicians = ['Harry Houdini', 'David Blaine', 'Teller']
+show_magicians(magicians)
+
+print("\nGreat magicians:")
+great_magicians = make_great(magicians[:])
+show_magicians(great_magicians)
+
+print("\nOriginal magicians:")
+show_magicians(magicians)
+
+def make_sandwich(*items):
+    """Make a sandwich with the given items."""
+    print("\nI'll make you a great sandwich:")
+    for item in items:
+        print("  ...adding " + item + " to your sandwich.")
+    print("Your sandwich is ready!")
+
+make_sandwich('roast beef', 'cheddar cheese', 'lettuce', 'honey dijon')
+make_sandwich('turkey', 'apple slices', 'honey mustard')
+make_sandwich('peanut butter', 'strawberry jam')
+
+def make_car(manufacturer, model, **options):
+    """Make a dictionary representing a car."""
+    car_dict = {
+        'manufacturer': manufacturer.title(),
+        'model': model.title(),
+        }
+    for option, value in options.items():
+        car_dict[option] = value
+
+    return car_dict
+
+my_outback = make_car('subaru', 'outback', color='blue', tow_package=True)
+print(my_outback)
+
+my_accord = make_car('honda', 'accord', year=1991, color='white',
+        headlights='popup')
+print(my_accord)
+
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until there are none left.
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+
+        # Simulate creating a 3d print from the design.
+        print("Printing model: " + current_design)
+        completed_models.append(current_design)
+
+
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
